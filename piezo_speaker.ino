a3e88d05722255844f60c4af499dcd09f66b31bb
RupkathaManzur <56570461+RupkathaManzur@users.noreply.github.com>
2019-11-07 23:53:55 -0600

    Add files via upload

---
+++ piezo_speaker.ino
@@ -0,0 +1,18 @@
+/*CHALLENGE
+CS lab 2 
+Piezo speaker
+A sketch to demonstrate the tone() function*/
+const int piezoPin = 9; //the output pin
+const int frequency = 70; //the pitch of the noise 
+void setup() {
+  // put your setup code here, to run once:
+   pinMode(piezoPin, OUTPUT); 
+
+}
+
+void loop(){
+ 
+  tone(piezoPin,frequency); 
+  delay (500);  //the tone play time measured in milliseconds    
+  
+}
