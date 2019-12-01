7fc1a5ff20d5d197effa25bf4084686cd561261f
RupkathaManzur <56570461+RupkathaManzur@users.noreply.github.com>
2019-11-30 18:33:15 -0600

    Add files via upload
    As I am planning to modify an Arduino based smart Dustbin. The actual project is done by the admin of the website Techatronics.com. In the original project, the dustbin has the ability to open the lid on its own when the ultrasonic sensor attached to it, senses presence like a human or any other object within a certain distance. This code is for opening the lid with the help of an ultrasonic sensor and servo.

---
+++ Testing_Ultrasonic_sensor_with_the_servo_2.ino
@@ -0,0 +1,47 @@
+/*This sketch is for the second ultrasonic sensor to control the servo. 
+When the ultrasonic sensor senses motion it initiates the servo which helps to open the the lid of the smart dustbin.*/
+#include <Servo.h>
+
+Servo myServo;
+
+int trigPin = 4; // Initializing the pins of the ultrasonic sensor
+int echoPin = 5;
+
+long distance;
+long duration;
+ 
+void setup() 
+
+{
+   
+  myServo.attach(9); 
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);  
+}
+
+
+ 
+void loop(){
+  ultra();
+  myServo.write(0);
+  if(distance <= 10) // the servo will open the lid when it senses motion in less than 10 cm
+  {
+  myServo.write(90);
+  }
+  }
+
+ 
+void ultra()
+  {
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW); // to cntrol the time between opening and closing of the dustbin.
+  duration = pulseIn(echoPin, HIGH);
+  distance = duration*0.034/2;
+  }
+
+  /* This code is from the main project of the Smart Dustbin of the guys Techatronics.com with a few changes made according to the size and feature of the dustbin.
+  citation
+  Admin. “Smart Dustbin using Arduino”. Techatronic.com. May 14, 2019.*/
