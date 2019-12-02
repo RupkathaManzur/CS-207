3cb1c12a9ace0748f9e2daae38c6c8963b8070b1
RupkathaManzur <56570461+RupkathaManzur@users.noreply.github.com>
2019-12-02 03:39:18 -0600

    Update Testing_Ultrasonic_sensor1__with_buzzer.ino

--- Testing_Ultrasonic_sensor1__with_buzzer.ino
+++ Testing_Ultrasonic_sensor1__with_buzzer.ino
@@ -3,9 +3,9 @@ In the smart dustbin the work of one of the ultrasonic sensor is to emit a sound
 the sketch below we will be able to fix the distance upto which we want the garbage to be full using a serial monitor.*/
 
 
-int trig_pin = 4;  // initialization of the trigger pin and echo for the ultrasonic sensor. 
+int trig_pin = 2;  // initialization of the trigger pin and echo for the ultrasonic sensor. 
                              
-int echo_pin = 5;
+int echo_pin = 3;
 
 int buzzer_pin = 10; // initialized the buzzer pin
 
